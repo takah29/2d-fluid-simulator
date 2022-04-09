a41700171c2617d9cb984b563ce2ee8f192e2409
takah29 <takah0209@gmail.com>
2022-04-10 03:45:37 +0900

    swapができていなかったのでfieldクラスを引数として渡すようにした、ベクトルの角度と大きさをRGBへ変換する関数を追加した

--- fluid_sim.py
+++ fluid_sim.py
@@ -1,3 +1,4 @@
+from math import pi
 import numpy as np
 
 import taichi as ti
@@ -9,11 +10,11 @@ ti.init(arch=arch)
 class DoubleBuffers:
     def __init__(self, resolution, n_channel):
         if n_channel == 1:
-            self.current = ti.field(float, shape=(resolution, resolution))
-            self.next = ti.field(float, shape=(resolution, resolution))
+            self.current = ti.field(float, shape=(2 * resolution, resolution))
+            self.next = ti.field(float, shape=(2 * resolution, resolution))
         else:
-            self.current = ti.Vector.field(n_channel, float, shape=(resolution, resolution))
-            self.next = ti.Vector.field(n_channel, float, shape=(resolution, resolution))
+            self.current = ti.Vector.field(n_channel, float, shape=(2 * resolution, resolution))
+            self.next = ti.Vector.field(n_channel, float, shape=(2 * resolution, resolution))
 
     def swap(self):
         self.current, self.next = self.next, self.current
@@ -25,7 +26,7 @@ class DoubleBuffers:
 
 @ti.data_oriented
 class FluidSimulator:
-    def __init__(self, resolution, dt=0.03, Re=1.54, p_iter=50):
+    def __init__(self, resolution, dt=0.01, Re=5.54, p_iter=50):
         self._resolution = resolution
         self.dt = dt
         self.Re = Re
@@ -37,18 +38,19 @@ class FluidSimulator:
         self.p_iter = p_iter
         self.g = ti.Vector([0, -9.8])
 
-    def step(self):
-        self._set_bc()
-        self._update_velocities()
-        print(self.v.current[1, 100])
+        self.v.current.fill(ti.Vector([0.4, 0.0]))
 
+    def step(self):
+        self._set_bc(self.v.current)
+        self._update_velocities(self.v.current, self.v.next, self.p.current)
         self.v.swap()
 
+        self._set_bc(self.v.current)
         for _ in range(self.p_iter):
-            self._update_pressures()
+            self._update_pressures(self.p.current, self.p.next, self.v.current)
             self.p.swap()
 
-        self._to_buffer()
+        self._to_buffer(self.buf.current, self.v.current, self.p.current)
 
     def set_boundary_condition(self, boundary_condition):
         self.bc = boundary_condition
@@ -57,56 +59,57 @@ class FluidSimulator:
         return self.buf.current
 
     @ti.kernel
-    def _update_velocities(self):
-        for i, j in self.v.next:
-            self.v.next[i, j] = self.v.current[i, j] + self.dt * (
-                -self.v.current[i, j].x * self._diff_x(self.v.current, i, j)
-                - self.v.current[i, j].y * self._diff_y(self.v.current, i, j)
+    def _update_velocities(self, vc: ti.template(), vn: ti.template(), pc: ti.template()):
+        for i, j in vn:
+            vn[i, j] = vc[i, j] + self.dt * (
+                -vc[i, j].x * self._diff_x(vc, i, j)
+                - vc[i, j].y * self._diff_y(vc, i, j)
                 - ti.Vector(
                     [
-                        self._diff_x(self.p.current, i, j),
-                        self._diff_y(self.p.current, i, j),
+                        self._diff_x(pc, i, j),
+                        self._diff_y(pc, i, j),
                     ]
                 )
-                + (self._diff2_x(self.v.current, i, j) + self._diff2_y(self.v.current, i, j))
-                / self.Re
+                + (self._diff2_x(vc, i, j) + self._diff2_y(vc, i, j)) / self.Re
             )
 
     @ti.kernel
-    def _update_pressures(self):
-        for i, j in self.p.next:
-            self.p.next[i, j] = (
+    def _update_pressures(self, pc: ti.template(), pn: ti.template(), vc: ti.template()):
+        for i, j in pn:
+            pn[i, j] = (
                 (
-                    self._sample(self.p.current, i + 1, j)
-                    + self._sample(self.p.current, i - 1, j)
-                    + self._sample(self.p.current, i, j + 1)
-                    + self._sample(self.p.current, i, j - 1)
+                    self._sample(pc, i + 1, j)
+                    + self._sample(pc, i - 1, j)
+                    + self._sample(pc, i, j + 1)
+                    + self._sample(pc, i, j - 1)
                 )
-                - (self._diff_x(self.v.current, i, j).x + self._diff_y(self.v.current, i, j).y)
-                / self.dt
-                + self._diff_x(self.v.current, i, j).x ** 2
-                + self._diff_y(self.v.current, i, j).y ** 2
-                + 2 * self._diff_y(self.v.current, i, j).x * self._diff_x(self.v.current, i, j).y
+                - (self._diff_x(vc, i, j).x + self._diff_y(vc, i, j).y) / self.dt
+                + self._diff_x(vc, i, j).x ** 2
+                + self._diff_y(vc, i, j).y ** 2
+                + 2 * self._diff_y(vc, i, j).x * self._diff_x(vc, i, j).y
             ) * 0.25
 
     @ti.kernel
-    def _set_bc(self):
-        for i, j in self.v.current:
+    def _set_bc(self, vc: ti.template()):
+        for i, j in vc:
             if (self.bc[i, j] >= ti.Vector([0.0, 0.0])).all():
-                self.v.current[i, j] = self.bc[i, j]
-                self.v.next[i, j] = self.bc[i, j]
+                vc[i, j] = self.bc[i, j]
+            if i == (2 * self._resolution - 1) and j != 0 and j != self._resolution - 1:
+                vc[i, j] = vc[i - 1, j]
 
     @ti.kernel
-    def _to_buffer(self):
-        for i, j in self.buf.current:
-            self.buf.current[i, j].x = 256.0 * self.v.current[i, j].x
-            self.buf.current[i, j].y = 256.0 * self.v.current[i, j].y
-            self.buf.current[i, j].z = 256.0 * self.p.current[i, j]
+    def _to_buffer(self, bufc: ti.template(), vc: ti.template(), pc: ti.template()):
+        for i, j in bufc:
+            # bufc[i,j] = self._to_rgb(vc[i,j])
+            bufc[i, j].x = ti.abs(vc[i, j].y)
+            bufc[i, j].y = 0.0001 * pc[i, j]
+            bufc[i, j].z = ti.abs(vc[i, j].x)
 
     @ti.func
     def _sample(self, field, i, j):
+        i = max(0, min(2 * self._resolution - 1, i))
+        j = max(0, min(self._resolution - 1, j))
         idx = ti.Vector([int(i), int(j)])
-        idx = max(0, min(self._resolution - 1, idx))
         return field[idx]
 
     @ti.func
@@ -133,43 +136,80 @@ class FluidSimulator:
             + self._sample(field, i, j - 1)
         )
 
+    @ti.func
+    def _to_rgb(self, vec):
+        h = (ti.atan2(vec.y, vec.x) + pi) / (2 * pi)
+        s = 1.0
+        v = ti.sqrt(vec.x**2 + vec.y**2)
+
+        r = v
+        g = v
+        b = v
+
+        h *= 6.0
+        i = int(h)
+        f = h - float(i)
+        if i == 0:
+            g *= 1 - s * (1 - f)
+            b *= 1 - s
+        elif i == 1:
+            r *= 1 - s * f
+            b *= 1 - s
+        elif i == 2:
+            r *= 1 - s
+            b *= 1 - s * (1 - f)
+        elif i == 3:
+            r *= 1 - s
+            g *= 1 - s * f
+        elif i == 4:
+            r *= 1 - s * (1 - f)
+            g *= 1 - s
+        elif i == 5:
+            g *= 1 - s
+            b *= 1 - s * f
+
+        return ti.Vector([r, g, b])
+
 
 def create_bc(resolution):
-    bc = -np.ones((resolution, resolution, 2))
+    bc = -np.ones((2 * resolution, resolution, 2))
+    size = resolution // 18
 
     # 流入部、流出部の設定
-    bc[0, :] = np.array([1.0, 0.0])
-    bc[-1, :] = np.array([1.0, 0.0])
+    bc[0, :] = np.array([0.4, 0.0])
+    bc[0, resolution // 2 - 2 * size : resolution // 2 + 2 * size] = np.array([4.0, 0.0])
+
+    # bc[-1, :] = np.array([2.0, 0.0])
 
     # 壁の設定
     bc[:, 0] = np.array([0.0, 0.0])
     bc[:, -1] = np.array([0.0, 0.0])
-    size = resolution // 6
     bc[
         resolution // 2 - 2 * size : resolution // 2,
         resolution // 2 - size : resolution // 2 + size,
     ] = np.array([0.0, 0.0])
 
-    bc_field = ti.Vector.field(2, float, shape=(resolution, resolution))
+    bc_field = ti.Vector.field(2, float, shape=(2 * resolution, resolution))
     bc_field.from_numpy(bc)
 
     return bc_field
 
 
 def main():
-    resolution = 1000
-    max_fps = 60
-
+    resolution = 500
     paused = False
     debug = False
 
-    window = ti.ui.Window("Fluid Simulation", (resolution, resolution), vsync=True)
+    window = ti.ui.Window("Fluid Simulation", (2 * resolution, resolution), vsync=False)
     canvas = window.get_canvas()
 
     fluid_sim = FluidSimulator(resolution)
     bc = create_bc(resolution)
     fluid_sim.set_boundary_condition(bc)
 
+    video_manager = ti.VideoManager(output_dir="result", framerate=30, automatic_build=False)
+
+    count = 0
     while window.running:
         if window.get_event(ti.ui.PRESS):
             e = window.event
@@ -183,8 +223,16 @@ def main():
         if not paused:
             fluid_sim.step()
 
-        canvas.set_image(fluid_sim.get_buffer())
+        img = fluid_sim.get_buffer()
+        canvas.set_image(img)
         window.show()
+
+        # if count % 100 == 0:
+        #     video_manager.write_frame(img)
+
+        count += 1
+
+    # video_manager.make_video(mp4=True)
 
 
 if __name__ == "__main__":
