d9872750494e86f553205187be21b070f894c783
takah29 <takah0209@gmail.com>
2022-04-18 01:20:10 +0900

    ENO(Essentially Non-Oscillatory polynomial interpolation)を追加

--- fluid_sim.py
+++ fluid_sim.py
@@ -181,6 +181,86 @@ class FluidSimulator:
         return a + b
 
     @ti.func
+    def _advect_eno(self, vc, i, j):
+        """ENO(Essentially Non-Oscillatory polynomial interpolation)
+
+        http://www.slis.tsukuba.ac.jp/~fujisawa.makoto.fu/cgi-bin/wiki/index.php?%B0%DC%CE%AE%CB%A1#ufe6b856
+        """
+        c2 = c3 = ti.Vector([0.0, 0.0])
+        k = k_star = 0
+        # calc advect_x
+        # first order
+        if vc[i, j].x < 0.0:
+            k = i
+        else:
+            k = i - 1
+        Dk_1 = self._sample(vc, k + 1, j) - self._sample(vc, k, j)
+        qx1 = Dk_1
+
+        # second order
+        Dk_2 = self._diff2_x(vc, k, j) / 2.0
+        Dk1_2 = self._diff2_x(vc, k + 1, j) / 2.0
+
+        if ti.abs(Dk_2.x) <= ti.abs(Dk1_2.x):
+            c2 = Dk_2
+        else:
+            c2 = Dk1_2
+
+        qx2 = c2 * (2 * (i - k) - 1)
+
+        # third order
+        if ti.abs(Dk_2.x) <= ti.abs(Dk1_2.x):
+            k_star = k - 1
+        else:
+            k_star = k
+        Dk_star_3 = self._diff3_x(vc, k_star, j) / 6.0
+        Dk_star1_3 = self._diff3_x(vc, k_star + 1, j) / 6.0
+
+        if ti.abs(Dk_star_3.x) <= ti.abs(Dk_star1_3.x):
+            c3 = Dk_star_3
+        else:
+            c3 = Dk_star1_3
+
+        qx3 = c3 * (3 * (i - k_star) ** 2 - 6 * (i - k_star) + 2)
+
+        advect_x = vc[i, j].x * (qx1 + qx2 + qx3)
+
+        # calc advect_y
+        # first order
+        if vc[i, j].y < 0.0:
+            k = j
+        else:
+            k = j - 1
+        Dk_1 = self._sample(vc, i, k + 1) - self._sample(vc, i, k)
+        qy1 = Dk_1
+
+        # second order
+        Dk_2 = self._diff2_y(vc, i, k) / 2.0
+        Dk1_2 = self._diff2_y(vc, i, k + 1) / 2.0
+        if ti.abs(Dk_2.y) <= ti.abs(Dk1_2.y):
+            c2 = Dk_2
+        else:
+            c2 = Dk1_2
+        qy2 = c2 * (2 * (j - k) - 1)
+
+        # third order
+        if ti.abs(Dk_2.y) <= ti.abs(Dk1_2.y):
+            k_star = k - 1
+        else:
+            k_star = k
+        Dk_star_3 = self._diff3_y(vc, i, k_star) / 6.0
+        Dk_star1_3 = self._diff3_y(vc, i, k_star + 1) / 6.0
+        if ti.abs(Dk_star_3.y) <= ti.abs(Dk_star1_3.y):
+            c3 = Dk_star_3
+        else:
+            c3 = Dk_star1_3
+        qy3 = c3 * (3 * (j - k_star) ** 2 - 6 * (j - k_star) + 2)
+
+        advect_y = vc[i, j].y * (qy1 + qy2 + qy3)
+
+        return advect_x + advect_y
+
+    @ti.func
     def _sample(self, field, i, j):
         i = max(0, min(self._resolution[0] - 1, i))
         j = max(0, min(self._resolution[1] - 1, j))
