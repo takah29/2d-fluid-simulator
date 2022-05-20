1e936bcd217e1b423e68df898a4ad9b04162ab1b
takah29 <takah0209@gmail.com>
2022-05-21 06:27:52 +0900

    不要なインポートを削除

--- advection.py
+++ advection.py
@@ -1,18 +1,6 @@
 import taichi as ti
 
-from differentiation import (
-    sample,
-    diff_x,
-    diff_y,
-    fdiff_x,
-    fdiff_y,
-    bdiff_x,
-    bdiff_y,
-    diff2_x,
-    diff2_y,
-    diff3_x,
-    diff3_y,
-)
+from differentiation import diff_x, diff_y, fdiff_x, fdiff_y, sample
 
 
 @ti.func
