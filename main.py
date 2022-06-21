bbd3308ba28b5de7945545cc3bfcf37d5aa306a9
houkensjtu <houkensjtu@gmail.com>
2022-06-21 20:15:44 +0800

    境界条件6を追加

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
         "--boundary_condition",
         help="Boundary condition number",
         type=int,
-        choices=[1, 2, 3, 4, 5],
+        choices=[1, 2, 3, 4, 5, 6],
         default=1,
     )
     parser.add_argument("-re", "--reynolds_num", help="Reynolds number", type=float, default=10.0)
