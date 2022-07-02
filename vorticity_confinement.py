fc49253d16f9f34db80c01d9f693f19e2f71ae91
takah29 <takah0209@gmail.com>
2022-07-02 22:23:13 +0900

    VorticityConfinementをクラス化

---
+++ vorticity_confinement.py
@@ -0,0 +1,50 @@
+import taichi as ti
+
+from differentiation import diff_x, diff_y
+
+
+@ti.data_oriented
+class VorticityConfinement:
+    def __init__(self, boundary_condition, dt, weight):
+        self._bc = boundary_condition
+        self.dt = dt
+        self.weight = weight
+
+        self._resolution = boundary_condition.get_resolution()
+
+        self.vorticity = ti.field(float, shape=self._resolution)
+        self.vorticity_abs = ti.field(float, shape=self._resolution)
+
+    @ti.kernel
+    def _calc_vorticity(self, vc: ti.template()):
+        for i, j in self.vorticity:
+            if not self._bc.is_wall(i, j):
+                self.vorticity[i, j] = diff_x(vc, i, j).y - diff_y(vc, i, j).x
+                self.vorticity_abs[i, j] = ti.abs(self.vorticity[i, j])
+
+    @ti.kernel
+    def _add_vorticity(
+        self,
+        vn: ti.template(),
+        vc: ti.template(),
+    ):
+        for i, j in vn:
+            if not self._bc.is_wall(i, j):
+                vn[i, j] = vc[i, j] + self.dt * self.weight * self._vorticity_vec(i, j)
+
+    @ti.func
+    def _vorticity_vec(self, i, j):
+        vorticity_grad_vec = ti.Vector(
+            [diff_x(self.vorticity_abs, i, j), diff_y(self.vorticity_abs, i, j)]
+        )
+        vorticity_grad_vec = vorticity_grad_vec / vorticity_grad_vec.norm()
+        vorticity_vec = (
+            ti.Vector([vorticity_grad_vec.y, -vorticity_grad_vec.x]) * self.vorticity[i, j]
+        )
+
+        # 発散する可能性があるのでクランプする
+        return ti.max(ti.min(vorticity_vec, 0.1), -0.1)
+
+    def apply(self, v):
+        self._calc_vorticity(v.current)
+        self._add_vorticity(v.next, v.current)  # v.nextのみ更新する
